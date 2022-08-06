65a5d4c8afa5d523c1aba5c886b3ad951e64d00e
Seva-Sh <skimin1992@hotmail.com>
2022-08-06 16:24:53 +0400

    Initial UI Build Completed

--- code.js
+++ code.js
@@ -1,8 +1,12 @@
-function getPlayerChoice() {
-    let choice = prompt("Please type : 'Rock', 'Paper', 'Scissors");
-    choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-    return choice;
-}
+const buttons = document.querySelectorAll('button');
+const plScore = document.querySelector('#plScore');
+const comScore = document.querySelector('#comScore');
+const disWinner = document.querySelector('#displayWinner');
+const comSelection = document.querySelector('#computerSelection');
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice () {
     const choices = ["Rock", "Paper", "Scissors"];
@@ -23,41 +27,67 @@ function playRound (playerSelection, computerSelection) {
 }
 
 function displayScore (playerScore, computerScore) {
-    console.log(`Player Score: ${playerScore}`);
-    console.log(`Computer Score: ${computerScore}`);
+    plScore.textContent = `Player Score: ${playerScore}`;
+    comScore.textContent = `Computer Score: ${computerScore}`
 }
 
-function displayWinner (playerScore, computerScore) {
-    if (playerScore > computerScore) {
-        console.log("You win the game!");
-    } else if (computerScore > playerScore) {
-        console.log("Computer wins this game!");
-    } else {
-        console.log("Friendship wins!");
-    }
+function displayComputerSelection(computerSelection) {
+    comSelection.textContent = `Computer selected: ${computerSelection}`;
 }
 
-function playGame() {
-    
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection =  getComputerChoice();
-    
-        const result = playRound(playerSelection, computerSelection);
-
-        const losingChoice = result.split(" ").at(-1);
-        if (playerSelection != computerSelection) {
-            playerSelection == losingChoice ? computerScore += 1 : playerScore += 1
+function displayWinner (playerScore, computerScore, result) {
+    if (playerScore >= 5 || computerScore >= 5) {
+        if (playerScore > computerScore) {
+            disWinner.textContent = "You win the game!";
+        } else if (computerScore > playerScore) {
+            disWinner.textContent = "Computer wins this game!";
+        } else {
+            disWinner.textContent = "Friendship wins!";
         }
-
-        console.log(`Computer selected: ${computerSelection}`);
-        console.log(result);
-        displayScore(playerScore, computerScore);
-    }
-    displayWinner(playerScore, computerScore);
+    } else {
+        disWinner.textContent = result;
+    } 
 }
 
-playGame();
+function playGame(playerSelection) {
+    
+    const computerSelection =  getComputerChoice();
+    
+    const result = playRound(playerSelection, computerSelection);
+
+    const losingChoice = result.split(" ").at(-1);
+    if (playerSelection != computerSelection) {
+         playerSelection == losingChoice ? computerScore += 1 : playerScore += 1
+    }
+
+    displayScore(playerScore, computerScore);
+    displayComputerSelection(computerSelection);
+    displayWinner(playerScore, computerScore, result);
+
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.id}"]`);
+    const button = document.querySelector(`button[id="${e.id}"]`);
+
+    audio.currentTime = 0;
+    audio.play();
+    button.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName != 'transform') return;
+    this.classList.remove('playing');
+}
+
+displayScore(playerScore, computerScore);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+        playSound(button);
+        button.addEventListener('transitionend', removeTransition)
+        playGame(button.id);
+
+    });
+});
